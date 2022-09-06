0ca054e337ba052bf95a3cc9d628d1862c65c26a
HZ1Q <33613789+HZ1Q@users.noreply.github.com>
2022-09-06 10:13:29 -0400

    Update download.bat

--- download.bat
+++ download.bat
@@ -1,2 +1,3 @@
 curl -L https://raw.githubusercontent.com/HZ1Q/DRCMA/main/DRCMA.bat -o C:\Users\%Username%\Desktop\DRCMA_Installer.bat
+call notepad C:\Users\%Username%\Desktop\DRCMA_Installer.bat
 call C:\Users\%Username%\Desktop\DRCMA_Installer.bat
