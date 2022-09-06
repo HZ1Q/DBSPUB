0258b48e9ae303a9dd32c1ec911b8eb9479f92fb
HZ1Q <33613789+HZ1Q@users.noreply.github.com>
2022-09-06 09:57:55 -0400

    Download the DRCMA_Installer.bat script

--- download.bat
+++ download.bat
@@ -1,1 +1,4 @@
-curl -L https://raw.githubusercontent.com/HZ1Q/DRCMA/main/DRCMA.bat -o C:\Users\%Username%\Desktop\DRCMA_Installer.bat
+set outputpath=C:\Users\%Username%\Desktop\DRCMA_Installer.bat
+curl -L https://raw.githubusercontent.com/HZ1Q/DRCMA/main/DRCMA.bat -o %outputpath%
+echo The DRCMA_Installer.bat has been stored at the below location:
+%outputpath%
