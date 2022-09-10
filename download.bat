1dc2857c8f855f0aeea72165820d13fd71a29544
HZ1Q <33613789+HZ1Q@users.noreply.github.com>
2022-09-09 20:27:50 -0400

    Delete download.bat

--- download.bat
+++
@@ -1,2 +0,0 @@
-rem https://bit.ly/dbs_drcma redirects to https://raw.githubusercontent.com/HZ1Q/DBS/main/DRCMA/DRCMA.bat
-curl -L https://bit.ly/dbs_drcma -o C:\Users\%Username%\Desktop\DRCMA_Installer.bat
